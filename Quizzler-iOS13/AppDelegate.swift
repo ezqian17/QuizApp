71b8f0968ee4b2e4237a5f1f28edba96112f49fb
ezqian17 <36429905+ezqian17@users.noreply.github.com>
2023-03-09 12:39:23 -0600

    Added context

--- Quizzler-iOS13/AppDelegate.swift
+++ Quizzler-iOS13/AppDelegate.swift
@@ -9,6 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    //terminal
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
